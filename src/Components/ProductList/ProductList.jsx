26fcc615e596d230da7b4503f8c9e0bddbac6aa4
Kati Hofstetter <102166971+katrinhofstetter@users.noreply.github.com>
2022-03-23 08:59:27 +0100

    try to ix eslint flowtype error

--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -6,12 +6,12 @@ const ProductList = () => {
     <>
       <h1>Our Products</h1>
       <div className='product__list'>
-        <div>Product 1</div>
-        <div>Product 2</div>
-        <div>Product 3</div>
-        <div>Product 4</div>
-        <div>Product 5</div>
-        <div>Product 6</div>
+        <div className='product__item'>Product 1</div>
+        <div className='product__item'>Product 2</div>
+        <div className='product__item'>Product 3</div>
+        <div className='product__item'>Product 4</div>
+        <div className='product__item'>Product 5</div>
+        <div className='product__item'>Product 6</div>
       </div>
     </>
   );
